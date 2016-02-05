7b63c8d0dd898cfed1e93c2cc5a12c3cdf55f60a
BenedictRM <mehringr@gmail.com>
2016-02-05 14:49:51 -0700

    Matching navbar to original parking app
    Just calling our old map instead of trying to create something in react

--- src/apps/parking/react/components/navbar.js
+++ src/apps/parking/react/components/navbar.js
@@ -5,10 +5,10 @@ MyComponents.NavBar = React.createClass({
         <div className="nav-wrapper green lighten-2">
         <a href="#" className="brand-logo center">Garage Viewer</a>
         <ul id="nav-mobile" className="left hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">JavaScript</a></li>
 		  <li><a href="/">Course Home</a></li>
+		  <li><a href="index.html">Home</a></li>
+		  <li><a href="garages.html">List</a></li>
+		  <li><a href="/apps/parking/garages_map.html">Map</a></li>
         </ul>
         </div>
       </nav>
