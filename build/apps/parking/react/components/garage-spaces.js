d174e27d15c0e2e11776ce76dc696aa2896e5923
BenedictRM <mehringr@gmail.com>
2016-02-05 16:25:10 -0700

    update nav bar
    nav bar

--- build/apps/parking/react/components/garage-spaces.js
+++ build/apps/parking/react/components/garage-spaces.js
@@ -1,10 +1,21 @@
 MyComponents.GarageSpaces = React.createClass({
   render: function() { 
     return (
-      <div className="card">
-        <div className="card-content">
-          Open Space: {this.props.open} <div></div>
-		  Total Space: {this.props.total}
+      <div className="row">
+        <div className="col s12 m9">
+          <div className="card">
+            <div className="card-image waves-effect waves-block waves-light">
+              <img className="activator" src="https://c1.staticflickr.com/3/2790/4540289776_65c401d463_z.jpg" height="500"></img>
+            </div>
+            <div className="card-content">
+              <span className="card-title activator grey-text text-darken-4">Parking Spaces<i className="material-icons right">more_vert</i></span>
+            </div>
+            <div className="card-reveal">
+              <span className="card-title grey-text text-darken-4">Parking Spaces:<i className="material-icons right">close</i></span>
+              <p>Open Spaces: {this.props.open}</p>
+              <p>Total Spaces: {this.props.total}</p>
+            </div>
+          </div>
         </div>
       </div>
     );
