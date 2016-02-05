ac72bb75914248a29680cfd0530a49871aba1a7f
BenedictRM <mehringr@gmail.com>
2016-02-05 14:28:03 -0700

    adjusting navbar
    here

--- build/apps/parking/react/components/navbar.js
+++ build/apps/parking/react/components/navbar.js
@@ -3,8 +3,8 @@ MyComponents.NavBar = React.createClass({
     return (
       <nav>
         <div className="nav-wrapper green lighten-2">
-        <a href="#" className="brand-logo">Garage Viewer</a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <a href="#" className="brand-logo center">Garage Viewer</a>
+        <ul id="nav-mobile" className="left hide-on-med-and-down">
           <li><a href="sass.html">Sass</a></li>
           <li><a href="badges.html">Components</a></li>
           <li><a href="collapsible.html">JavaScript</a></li>
