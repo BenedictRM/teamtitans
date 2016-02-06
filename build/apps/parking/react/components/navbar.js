4350f91d30b2f58f8f3f214ee5b403a1adf28e35
aokeson <Alexandra.Okeson@colorado.edu>
2016-02-05 19:30:26 -0700

    Fix nav bar links

--- build/apps/parking/react/components/navbar.js
+++ build/apps/parking/react/components/navbar.js
@@ -6,8 +6,8 @@ MyComponents.NavBar = React.createClass({
         <a href="#" className="brand-logo center"><font face="Book Antigua" size="9">Garage Viewer</font></a>
         <ul id="nav-mobile" className="left hide-on-med-and-down">
 		  <li><a href="/">Course Home</a></li>
-		  <li><a href="index.html">Home</a></li>
-		  <li><a href="garages.html">List</a></li>
+		  <li><a href="/apps/parking/index.html">Home</a></li>
+		  <li><a href="/apps/parking/garages.html">List</a></li>
 		  <li><a href="/apps/parking/garages_map.html">Map</a></li>
         </ul>
         </div>
